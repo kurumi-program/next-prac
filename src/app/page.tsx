b41ee40cca4a9de9acd4b867b77d7a778980ea58
kurumi.program <kurumi.program@gmail.com>
2025-07-24 23:12:59 +0900

    todo追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
 "use client";
 
+import { useState } from "react";
+
 export default function Home() {
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState([]);
+
   return (
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <h1 className="font-bold mt-3"></h1>
       <h2>TODOリスト</h2>
-      <input className="border" type="text" />
       <button className="border">追加</button>
+      <ul></ul>
     </div>
   );
 }
