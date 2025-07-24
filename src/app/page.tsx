eadaf2df3aa0866b47a275a01e7881a08226da78
kurumi.program <kurumi.program@gmail.com>
2025-07-24 23:24:17 +0900

    途中経過

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,22 @@
 import { useState } from "react";
 
 export default function Home() {
-  const [todo, setTodo] = useState<string>("");
+  const [task, setTask] = useState<string>("");
   const [todos, setTodos] = useState([]);
 
   return (
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <h1 className="font-bold mt-3"></h1>
       <h2>TODOリスト</h2>
-      <button className="border">追加</button>
+      <input
+        onChange={(e) => setTask(e.target.value)}
+        value={task}
+        className="border"
+        type="text"
+      />
+      <button onClick={() => console.log(task)} className="border">
+        追加
+      </button>
       <ul></ul>
     </div>
   );
